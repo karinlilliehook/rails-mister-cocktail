aeeba061b39eee9b503ade25903d47b965e0c24d
Karin Lilliehöök <karin@lilliehooks.se>
2018-11-01 19:04:51 +0100

    cocktails routes, conts and views done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -6,4 +6,29 @@ class CocktailsController < ApplicationController
   def show
     @cocktail = Cocktail.find(params[:id])
   end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
